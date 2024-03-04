be36036192e79843bbb575a613c8ff724d9cdc9d
Shoujian Zheng <zheng.shoujian@outlook.com>
2024-03-04 18:42:51 +0800

    chore: change the name of client pypi package name from 'plutolang' to 'pluto-client', publish the initial 'pluto-base' pypi package

---
+++ packages/pluto-py/pluto_client/queue.py
@@ -0,0 +1,6 @@
+from pluto_base.resource import IResource
+
+
+class Queue(IResource):
+    def push(self, item):
+        pass
--- packages/pluto-py/plutolang/queue.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Queue(ABC):
-    @abstractmethod
-    def push(self, item):
-        pass
