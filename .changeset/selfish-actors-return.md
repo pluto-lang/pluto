ea28a84998bf35f8d2907fc94c4a2f68c7a6e32d
Shoujian Zheng <zheng.shoujian@outlook.com>
2024-04-08 15:48:56 +0800

    feat(sdk): get aws region from pulumi stack configuration

---
+++ .changeset/selfish-actors-return.md
@@ -0,0 +1,5 @@
+---
+"@plutolang/pluto-infra": patch
+---
+
+feat(sdk): get aws region from pulumi stack configuration
--- packages/pluto-infra/src/aws/utils.ts
+++ packages/pluto-infra/src/aws/utils.ts
@@ -1,5 +1,13 @@
+import * as pulumi from "@pulumi/pulumi";
+
 export function currentAwsRegion(): string {
-  const region = process.env.AWS_REGION;
+  const awsConfig = new pulumi.Config("aws");
+  let region = awsConfig.get("region");
+  if (region !== undefined) {
+    return region;
+  }
+
+  region = process.env.AWS_REGION;
   if (region == undefined) {
     throw new Error("AWS_REGION is not defined");
   }
