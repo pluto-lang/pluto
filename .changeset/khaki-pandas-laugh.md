cb2065071e6e3de4071a41f65ea13f1d7baad66b
沧濯 <zhengshoujian.zsj@antgroup.com>
2024-07-15 17:24:28 +0800

    feat(base): add extraConfigs attribute for project-level config support
    Introduced an extraConfigs attribute in core components to enable additional project-level configurations.

---
+++ .changeset/khaki-pandas-laugh.md
@@ -0,0 +1,7 @@
+---
+"@plutolang/base": patch
+---
+
+feat(base): add extraConfigs attribute for project-level config support
+
+Introduced an extraConfigs attribute in core components to enable additional project-level configurations.
--- packages/base/core/base-component.ts
+++ packages/base/core/base-component.ts
@@ -3,6 +3,8 @@ import { Stack } from "../config";
 export interface BasicArgs {
   /** The project name. */
   readonly project: string;
+  /** The project level extra configurations. */
+  readonly extraConfigs?: Record<string, any>;
   /** The stack specified by user. */
   readonly stack: Stack;
   /** The root directory path of the current project. */
@@ -12,12 +14,14 @@ export interface BasicArgs {
 
 export abstract class BaseComponent {
   protected readonly project: string;
+  protected readonly extraConfigs?: Record<string, any>;
   protected readonly stack: Stack;
   protected readonly rootpath: string;
   protected readonly log?: (...data: any[]) => void;
 
   constructor(args: BasicArgs) {
     this.project = args.project;
+    this.extraConfigs = args.extraConfigs;
     this.stack = args.stack;
     this.rootpath = args.rootpath;
     this.log = args.log;
