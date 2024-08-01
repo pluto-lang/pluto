0dc407ecaf02d7e3a8b0960ee963fd94ad9cf01a
Jade Zheng <zheng.shoujian@outlook.com>
2024-08-01 13:31:13 +0800

    feat(sdk): add no-op infrastructure API to client SDK (#310)
    To prevent runtime errors, a no-op (no operation) implementation of the infrastructure API has been introduced in the client SDK. This ensures that when pytest is run locally, calls to the infrastructure API do not result in errors.
    
    Future plans may involve bundling the entire user code, not just the business logic, which further necessitates a no-op infrastructure API in the SDK to avoid runtime exceptions.

--- packages/base-py/pluto_base/resource.py
+++ packages/base-py/pluto_base/resource.py
@@ -1,5 +1,7 @@
 from abc import ABC
-from typing import Protocol
+from typing import Callable, List, Type
+
+_empty_lambda: Callable[..., None] = lambda *args, **kwargs: None
 
 
 class IResourceClientApi(ABC):
@@ -14,30 +16,103 @@ class IResourceCapturedProps(ABC):
     pass
 
 
-class _IResourceClient(Protocol, IResourceClientApi, IResourceCapturedProps):  # type: ignore
-    pass
-
-
 class IResource(ABC):
     fqn: str
     """
     Fully qualified name of the resource type. It should be same between the client and the infra
     sdk.
     """
-    _client: _IResourceClient
+    _client: IResourceClientApi | IResourceCapturedProps | None
     """
-    The client implemention of the resource for the specific cloud provider.
+    The client implemention of the resource for the specific cloud provider. 
+    
+    The type of this attribute should be (IResourceClientApi & IResourceCapturedProps), but mypy
+    doesn't support this.
+    """
+    _infra_iface: Type[IResourceInfraApi] | None
+    """
+    The interface that contains the infrastructure API of the resource. This is used to check if an
+    infrastructure API is called during runtime. If it is called, the call will be skipped.
     """
 
+    def __get_infra_apis(self) -> List[str]:
+        """
+        Get a list of infrastructure APIs defined in the resource's infrastructure interface.
+
+        Returns:
+            A list of strings representing the names of the user defined infrastructure APIs.
+
+        Raises:
+            ValueError: If multiple classes that inherit from IResourceInfraApi are found.
+        """
+
+        infra_iface: Type[IResourceInfraApi] | None = None
+
+        # Try to get the _infra_iface attribute from self, if user has set it, use it.
+        try:
+            infra_iface = self._infra_iface
+        except:
+            pass
+
+        if infra_iface is None:
+            # If the _infra_iface attribute doesn't exist, try to find the class that is a subclass
+            # of IResourceInfraApi and not a subclass of IResourceClientApi or
+            # IResourceCapturedProps.
+
+            # First, get all the classes that are a subclass of IResourceInfraApi.
+            infra_iface_clses: List[Type[IResourceInfraApi]] = [
+                klass
+                for klass in self.__class__.mro()
+                if issubclass(klass, IResourceInfraApi)
+            ]
+
+            # Second, find the class that is a subclass of IResourceInfraApi and not a subclass of
+            # IResourceClientApi or IResourceCapturedProps
+            for klass in infra_iface_clses:
+                if klass == IResourceInfraApi:
+                    # Skip the IResourceInfraApi class itself.
+                    continue
+
+                if not issubclass(klass, IResourceClientApi) and not issubclass(
+                    klass, IResourceCapturedProps
+                ):
+                    if infra_iface is not None:
+                        # Found multiple classes that inherit from IResourceInfraApi.
+                        raise ValueError(
+                            "Multiple base classes that inherit from IResourceInfraApi are found."
+                        )
+                    infra_iface = klass
+
+            # Set the _infra_iface attribute to the found class.
+            if infra_iface is not None:
+                super().__setattr__("_infra_iface", infra_iface)
+
+        # If the _infra_iface attribute is still None, return an empty list.
+        if infra_iface is None:
+            return []
+
+        # Get all the user defined infrastructure APIs.
+        # RULE: The infrastructure APIs should not start with "__".
+        infra_apis = [
+            func
+            for func in dir(infra_iface)
+            if callable(getattr(infra_iface, func)) and not func.startswith("__")
+        ]
+        return infra_apis
+
     def __getattribute__(self, name: str):
-        if name == "fqn":
+        if name == "fqn" or name.startswith("_"):
             return super().__getattribute__(name)
+
+        # Check if the attribute is an infrastructure API, if it is, return an empty lambda.
+        infra_apis = self.__get_infra_apis()
+        if name in infra_apis:
+            return _empty_lambda
 
         # Try to get the attribute from the client, if it doesn't exist, return the attribute of
         # self. This is to make sure that the client is the first priority.
         try:
-            client = super().__getattribute__("_client")
-            return getattr(client, name)
+            return getattr(self._client, name)
         except:
             # If the _client doesn't exist, or the attribute doesn't exist in the client, return the
             # attribute of self.
